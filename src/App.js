3a0309ccf7d2d3711de7b98c50ef799e8985fdd4
HumbertoDevelop <humbertodev14@gmail.com>
2022-04-09 20:19:00 -0400

    Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 /** @format */
 
-import Footer from "./Components/Molecules/Footer";
-import Header from "./Components/Molecules/Header";
-import Contact from "./Components/Pages/Contact";
-import Home from "./Components/Pages/Home";
-import Projects from "./Components/Pages/Projects";
-import Skills from "./Components/Pages/Skills";
 import "./App.css";
 
 function App() {
 	
 	return (
-		<div className="App relative" >
-			<Header />
-			<Home />
-			<Projects />
-			<Skills />
-			<Contact />
-			<Footer />
+		<div className="App relative bg-zinc-900" >
+			
 		</div>
 	);
 }
